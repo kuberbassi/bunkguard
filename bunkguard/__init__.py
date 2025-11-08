7c9cdb5341cd8774d3f6cad566b68bde2477125a
Kuber Bassi <kuberbassi2007@gmail.com>
2025-11-08 14:14:35 +0530

    vercel.json fixed

--- bunkguard/__init__.py
+++ bunkguard/__init__.py
@@ -14,8 +14,8 @@ def create_app():
     """Create and configure an instance of the Flask application."""
     load_dotenv()
     
-    # 🎯 CONFIRM THIS IS SET: Explicitly define the static folder
-    app = Flask(__name__, static_folder='static', static_url_path='/static')
+    # 🎯 PERMANENT FIX: Tell Flask to look one directory up for the 'static' folder.
+    app = Flask(__name__, static_folder='../static') 
     
     # bunkguard/__init__.py
     app.secret_key = os.getenv("FLASK_SECRET_KEY")
