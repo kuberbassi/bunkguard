0d3ceee5271b5e9393f9b5d919595f4149dba55b
Kuber Bassi <kuberbassi2007@gmail.com>
2025-12-17 23:31:42 +0530

    fix1

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lo0k9649dcg"
+    "revision": "0.gmpnbpv6h6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
