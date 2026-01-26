946446b857bfae8fc19eb0faa83fdd6a48a3c45d
KuberBassi <kuberbassi2007@gmail.com>
2026-01-26 21:58:44 +0530

    latest satble fix

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.94rs3442n4s"
+    "revision": "0.nod6phil1jk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
