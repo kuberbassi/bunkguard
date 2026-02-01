0cf533c968ffc158f091c0fb0fb7a8659e815e78
KuberBassi <kuberbassi2007@gmail.com>
2026-02-01 23:30:18 +0530

    chore: finalize docker configuration and documentation

--- run.py
+++ run.py
@@ -32,5 +32,6 @@ if __name__ == '__main__':
         host='0.0.0.0',
         port=5000,
         use_reloader=True,
-        log_output=True
+        log_output=True,
+        allow_unsafe_werkzeug=True
     )
